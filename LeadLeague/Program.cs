a477aba1e9e7a46188597b9d3fe6c90ba235b5a5
Justyna <justyna.sienkiewicz93@gmail.com>
2024-10-27 14:32:24 +0100

    implementing serilog and seq

--- LeadLeague/Program.cs
+++ LeadLeague/Program.cs
@@ -1,18 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using LeadLeague;
+using Serilog;
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Logger.Create();
 
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+    builder.Services.AddProblemDetails();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.MapGet("/", () => "Hello world");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.MapGet("/", () => "Hello world");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
